e6ba2d7e592b135ed16eb7f394d6d7a83fa3a4ea
willianslc <willianslc94@gmail.com>
2026-04-08 20:10:59 -0300

    add comentário

--- exercícios-2/exe1.py
+++ exercícios-2/exe1.py
@@ -16,6 +16,6 @@ temperaturas.append(float(input("Informe a temperatura da sexta-feira: ")))
 temperaturas.append(float(input("Informe a temperatura da sabado: ")))
 temperaturas.append(float(input("Informe a temperatura da domingo: ")))
 
-media = sum(temperaturas) / 7
+media = sum(temperaturas) / 7 # A função sum, soma todos os números da lista automaticamente
 
 print(f"A média das temperaturas para essa semnana foi {media:.1f} e as temperaturas máximas e mínimas fora max de {max(temperaturas)} e min de {min(temperaturas)}")
